676dd5f04f89b9d7e3f4574d30f51e09790c3615
Keita Yamamoto <yamamoto@altonotes.co.jp>
2023-09-13 16:01:36 +0900

    Update

--- app/src/main/java/com/example/webviewcss/CustomWebView.kt
+++ app/src/main/java/com/example/webviewcss/CustomWebView.kt
@@ -2,64 +2,27 @@ package com.example.webviewcss
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webviewcss.ui.theme.TestData
-import com.example.webviewcss.ui.theme.WebViewCssTheme
 
-class MainActivity : ComponentActivity() {
-
-    var webView: WebView? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            WebViewCssTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyWebView()
-                }
-            }
-        }
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    @Composable
-    fun MyWebView() {
-        AndroidView(
-            factory = ::WebView,
-            update = { webView ->
-                this.webView = webView
-                webView.webViewClient = webViewClient
-                webView.settings.javaScriptEnabled = true
-                webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-                WebView.setWebContentsDebuggingEnabled(true)
-
-                load()
-            }
-        )
-    }
+class CustomWebView(val context: Context, val webView: WebView?) {
 
     fun load() {
         val htmlPath = "file:///android_asset/pr-offer-quest.html"
         webView?.loadUrl(htmlPath)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    fun setupWebView(webView: WebView) {
+        webView.webViewClient = webViewClient
+        webView.settings.javaScriptEnabled = true
+        // webViewのコンテンツがhttps、headタグに埋め込むcssがhttpであるため設定
+        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        WebView.setWebContentsDebuggingEnabled(true)
+    }
     fun appendStyleSheet(css: String) {
         val escapedCss = escapeForStringLiteral(css)
         var script = "var style = document.createElement('style');"
@@ -91,7 +54,7 @@ class MainActivity : ComponentActivity() {
         return result
     }
 
-    private fun readResourceString(context: Context, fileName: String): String? {
+    private fun readResourceString(fileName: String): String? {
         return context.assets.open(fileName).bufferedReader().use {
             it.readText()
         }
@@ -101,7 +64,7 @@ class MainActivity : ComponentActivity() {
         override fun onPageFinished(view: WebView, url: String) {
             super.onPageFinished(view, url)
 
-            val css = readResourceString(context = this@MainActivity,"pr-offer-quest.css")
+            val css = readResourceString("pr-offer-quest.css")
             if (css != null) {
                 appendStyleSheet(css)
             }
